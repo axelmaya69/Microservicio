97c953da76dcf0bcb148448c4f02302e2b029ab1
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-04 21:11:13 -0600

    agragandonuevo microservicio y configurandolo como con el gateway

---
+++ MicroservicioPractica/micoservicio-eureka/src/main/java/com/microservice/eureka/micoservicio_eureka/MicoservicioEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.eureka.micoservicio_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicoservicioEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicoservicioEurekaApplication.class, args);
+	}
+
+}
---
+++ MicroservicioPractica/micoservicio-eureka/src/test/java/com/microservice/eureka/micoservicio_eureka/MicoservicioEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.eureka.micoservicio_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicoservicioEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
