d6170ee612a74bff07715dac0f53756db4b9b29d
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-04 21:03:30 -0600

    agragando gateway

---
+++ MicroservicioPractica/micoservicio-gateway/src/main/java/com/microservice/gateway/micoservicio_gateway/MicoservicioGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.gateway.micoservicio_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicoservicioGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicoservicioGatewayApplication.class, args);
+	}
+
+}
---
+++ MicroservicioPractica/micoservicio-gateway/src/test/java/com/microservice/gateway/micoservicio_gateway/MicoservicioGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.gateway.micoservicio_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicoservicioGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
