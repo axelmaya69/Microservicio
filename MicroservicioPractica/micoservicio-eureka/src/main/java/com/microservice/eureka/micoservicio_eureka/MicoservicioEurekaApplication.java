9cf9a6fc0175d3a65dd377284423f51f15b1baf2
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-05 21:48:36 -0600

    configuraciones finales

--- MicroservicioPractica/micoservicio-eureka/src/main/java/com/microservice/eureka/micoservicio_eureka/MicoservicioEurekaApplication.java
+++ MicroservicioPractica/micoservicio-eureka/src/main/java/com/microservice/eureka/micoservicio_eureka/MicoservicioEurekaApplication.java
@@ -2,8 +2,10 @@ package com.microservice.eureka.micoservicio_eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicoservicioEurekaApplication {
 
 	public static void main(String[] args) {
