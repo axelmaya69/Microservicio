6a840cdb698c8e3c31596c408afc9b5e3a38e677
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-05 21:33:47 -0600

    agrwgando nuevo microservicio config server

---
+++ MicroservicioPractica/microservicioConfigServer/src/main/java/com/microservicio/configServer/MicroservicioConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioConfigServerApplication.class, args);
+	}
+
+}
