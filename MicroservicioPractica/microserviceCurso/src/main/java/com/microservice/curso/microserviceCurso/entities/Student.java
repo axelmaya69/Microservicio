6584e0683c0d070f73cbdecb56039686c4be4d86
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-08 22:17:52 -0600

    agregando

--- MicroservicioPractica/microserviceCurso/src/main/java/com/microservice/curso/microserviceCurso/entities/Student.java
+++ MicroservicioPractica/microserviceCurso/src/main/java/com/microservice/curso/microserviceCurso/entities/Student.java
@@ -1,4 +1,31 @@
 package com.microservice.curso.microserviceCurso.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@Table(name = "Students")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+
+    private String name;
+
+    @Column(name = "last_Name")
+    private String lastName;
+
+    private String email;
+
+    @Column(name = "course_id")
+    private  Long courseId;
+
 }
