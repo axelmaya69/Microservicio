5dc2fe4ce4713d746db03f5109bed532ce87a256
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-09 21:34:13 -0600

    agregando interfaz servicio

---
+++ MicroservicioPractica/microserviceStudent/src/main/java/com/microservice/student/microserviceStudent/service/IStudentService.java
@@ -0,0 +1,16 @@
+package com.microservice.student.microserviceStudent.service;
+
+import com.microservice.student.microserviceStudent.entities.Student;
+
+import java.util.List;
+
+public interface IStudentService {
+
+    List<Student> findAll();
+
+    Student findById(Long Id);
+
+    void save (Student student);
+
+    List <Student> findByIdCourse(Long idCourse);
+}
