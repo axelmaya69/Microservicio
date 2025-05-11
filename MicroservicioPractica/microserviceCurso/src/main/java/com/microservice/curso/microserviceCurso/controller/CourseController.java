20ce2311edc01c530ff908bc51007a17deae6d28
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-10 21:40:59 -0600

    llenando clase controladora

--- MicroservicioPractica/microserviceCurso/src/main/java/com/microservice/curso/microserviceCurso/controller/CourseController.java
+++ MicroservicioPractica/microserviceCurso/src/main/java/com/microservice/curso/microserviceCurso/controller/CourseController.java
@@ -1,4 +1,35 @@
 package com.microservice.curso.microserviceCurso.controller;
 
+import com.microservice.curso.microserviceCurso.entities.Course;
+import com.microservice.curso.microserviceCurso.service.ICoursesServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/course")
 public class CourseController {
+
+    @Autowired
+    private ICoursesServices coursesServices;
+
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveCourse(@RequestBody Course course){
+        coursesServices.save(course);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllCourses(){
+        return ResponseEntity.ok(coursesServices.findAll());
+    }
+
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        return ResponseEntity.ok(coursesServices.findById(id));
+    }
+
 }
