a693cd65a374be040b993cb612986c421df2c992
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-10 21:46:05 -0600

    COMUNICANDO MICROSERVICIOS

--- MicroservicioPractica/microserviceStudent/src/main/java/com/microservice/student/microserviceStudent/controller/StudentController.java
+++ MicroservicioPractica/microserviceStudent/src/main/java/com/microservice/student/microserviceStudent/controller/StudentController.java
@@ -31,4 +31,8 @@ public class StudentController {
     return ResponseEntity.ok(studentService.findById(id));
     }
 
+    @GetMapping("/search-my-course/{idCourse}")
+    public ResponseEntity<?> findByIdCourse(@PathVariable Long idCourse){
+    return ResponseEntity.ok(studentService.findByIdCourse(idCourse));
+    }
 }
