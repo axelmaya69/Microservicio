87c78fc3a862305fe26a258c21fc4470edff830b
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-15 23:06:22 -0600

    Agregando

--- MicroservicioPractica/microserviceCurso/src/main/java/com/microservice/curso/microserviceCurso/client/IStudentClient.java
+++ MicroservicioPractica/microserviceCurso/src/main/java/com/microservice/curso/microserviceCurso/client/IStudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "microserviceStudent",url = "localhost:8080/api/student")
+@FeignClient(name = "microservice-student",url = "localhost:8080/api/student")
 public interface IStudentClient {
 
     @GetMapping("/search-my-course/{idCourse}")
