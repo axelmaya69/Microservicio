c1c8bea144c899a05e06d2b91eeaef650641c35d
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-16 22:58:11 -0600

    Agregando

--- MicroservicioPractica/micoservicio-gateway/src/main/java/com/microservice/gateway/micoservicio_gateway/MicoservicioGatewayApplication.java
+++ MicroservicioPractica/micoservicio-gateway/src/main/java/com/microservice/gateway/micoservicio_gateway/MicoservicioGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroservicioGatewayApplication {
+public class MicoservicioGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroservicioGatewayApplication.class, args);
+		SpringApplication.run(MicoservicioGatewayApplication.class, args);
 	}
 
 }
