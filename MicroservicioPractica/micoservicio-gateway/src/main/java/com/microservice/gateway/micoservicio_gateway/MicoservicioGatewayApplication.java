7facfdbd4133e82cdfcca46651ca902f8dfe36f8
axelmaya69 <axeldbmaya96@gmail.com>
2025-05-16 22:57:59 -0600

    Agregando

--- MicroservicioPractica/micoservicio-gateway/src/main/java/com/microservice/gateway/micoservicio_gateway/MicoservicioGatewayApplication.java
+++ MicroservicioPractica/micoservicio-gateway/src/main/java/com/microservice/gateway/micoservicio_gateway/MicoservicioGatewayApplication.java
@@ -2,13 +2,12 @@ package com.microservice.gateway.micoservicio_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class MicoservicioGatewayApplication {
+public class MicroservicioGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicoservicioGatewayApplication.class, args);
+		SpringApplication.run(MicroservicioGatewayApplication.class, args);
 	}
 
 }
